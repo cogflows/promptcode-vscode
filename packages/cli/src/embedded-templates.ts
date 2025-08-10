b4902b8d4b91b28a648585bc25bf06d07022930b
Asaf Rubin <rubin.asaf@gmail.com>
2025-08-10 14:17:40 +0300

    chore: update embedded-templates timestamp from build

--- packages/cli/src/embedded-templates.ts
+++ packages/cli/src/embedded-templates.ts
@@ -1,7 +1,7 @@
 /**
  * Embedded templates for compiled binaries.
  * This file is auto-generated during build - DO NOT EDIT MANUALLY.
- * Generated at: 2025-08-10T11:09:09.025Z
+ * Generated at: 2025-08-10T11:16:22.732Z
  */
 
 // Template contents embedded at build time
